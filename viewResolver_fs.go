c2e9ac1fece7d3772518500603e8f7bfb1548e06
michal-laskowski <1753681+michal-laskowski@users.noreply.github.com>
2025-02-19 18:39:27 +0100

    refactor

--- viewResolver_fs.go
+++ viewResolver_fs.go
@@ -9,48 +9,53 @@ import (
 	"strconv"
 )
 
-func NewFsViewResolver(fs fs.FS, options ...FsViewResolverOptions) ViewResolver {
+func NewFsViewResolver(fs fs.FS) ViewResolver {
 	return &viewResolver_fs{
-		fs: fs,
-		ext: []string{
-			".tsx",
-			".jsx",
-		},
+		fs:      fs,
+		resolve: simpleViewResolver(".tsx", ".jsx"),
+	}
+}
+
+func NewFsViewResolverCustom(fs fs.FS, r FSViewResolveFunc) ViewResolver {
+	return &viewResolver_fs{
+		fs:      fs,
+		resolve: r,
+	}
+}
+
+type FSViewResolveFunc = func(fs fs.FS, viewName string) (*url.URL, error)
+
+func simpleViewResolver(ext ...string) FSViewResolveFunc {
+	for _, e := range ext {
+		if e[0] != '.' {
+			panic("extension must start with dot")
+		}
+	}
+
+	return func(onFS fs.FS, viewName string) (*url.URL, error) {
+		for _, e := range ext {
+			f := viewName + e
+			if stat, err := fs.Stat(onFS, f); err != nil {
+				//continue
+			} else {
+				return url.ParseRequestURI("file:///" + f + "?ts=" + strconv.FormatInt(stat.ModTime().UnixMicro(), 16))
+			}
+		}
+		return nil, &os.PathError{
+			Op:   "not_found",
+			Path: viewName,
+			Err:  errors.New("could not resolve view file"),
+		}
 	}
 }
 
 type viewResolver_fs struct {
-	fs  fs.FS
-	ext []string
-}
-type FsViewResolverOptions func(*viewResolver_fs)
-
-func SearchExtensions(ext ...string) FsViewResolverOptions {
-	return func(e *viewResolver_fs) {
-		ext = []string{}
-		for _, e := range ext {
-			if e[0] != '.' {
-				panic("extension must start with dot")
-			}
-			ext = append(ext, e)
-		}
-	}
+	fs      fs.FS
+	resolve FSViewResolveFunc
 }
 
 func (this *viewResolver_fs) ResolveViewFile(viewName string) (*url.URL, error) {
-	for _, e := range this.ext {
-		f := viewName + e
-		if stat, err := fs.Stat(this.fs, f); err != nil {
-			//continue
-		} else {
-			return url.ParseRequestURI("file:///" + f + "?ts=" + strconv.FormatInt(stat.ModTime().UnixMicro(), 16))
-		}
-	}
-	return nil, &os.PathError{
-		Op:   "not_found",
-		Path: viewName,
-		Err:  errors.New("could not resolve view file"),
-	}
+	return this.resolve(this.fs, viewName)
 }
 
 func (this *viewResolver_fs) ResolveModuleFile(fromModule ModuleMeta, importPath string) (*url.URL, error) {
